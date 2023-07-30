190067f3d378d44c9a4872dddd90c9f30c4f215b
KeithG33 <keith.gordon9@gmail.com>
2023-07-30 14:48:20 -0700

    runningaverage works with list/dicts

--- utils.py
+++ utils.py
@@ -36,33 +36,49 @@ class RunningStats:
     def standard_deviation(self):
         # print(math.sqrt(self.variance()) + self.epsilon)
         return math.sqrt(self.variance()) + self.epsilon
-    
+
+
 class RunningAverage:
     def __init__(self):
         self.counts = {}
         self.averages = {}
 
-    def add(self, var_name):
-        if var_name not in self.averages:
-            self.averages[var_name] = 0.0
-            self.counts[var_name] = 0
+    def add(self, var_names):
+        var_names = [var_names] if isinstance(var_names, str) else var_names
+        for var_name in var_names:
+            if var_name not in self.averages:
+                self.averages[var_name] = 0.0
+                self.counts[var_name] = 0
 
-    def update(self, var_name, value):
-        if var_name not in self.averages:
-            print(f"Variable {var_name} is not being tracked. Use add method to track.")
-            return
-        self.counts[var_name] += 1
-        self.averages[var_name] += (value - self.averages[var_name]) / self.counts[var_name]
+    def update(self, var_name, value=None):
+        if isinstance(var_name, dict):
+            for k, v in var_name.items():
+                if k not in self.averages:
+                    print(f"Variable {k} is not being tracked. Use add method to track.")
+                    continue
+                self.update(k, v)
+        else:
+            if var_name not in self.averages:
+                print(f"Variable {var_name} is not being tracked. Use add method to track.")
+                return
+            self.counts[var_name] += 1
+            self.averages[var_name] += (value - self.averages[var_name]) / self.counts[var_name]
 
-    def get_average(self, var_name):
-        return self.averages.get(var_name, None)
+    def get_average(self, var_names):
+        if isinstance(var_names, str):
+            return self.averages.get(var_names, None)
 
-    def reset(self, var_name=None):
-        if var_name is None:
+        return {var_name: self.averages.get(var_name, None) for var_name in var_names}
+
+    def reset(self, var_names=None):
+        if var_names is None:
             self.counts = {}
             self.averages = {}
-        elif var_name in self.averages:
-            self.counts[var_name] = 0
-            self.averages[var_name] = 0.0
         else:
-            print(f"Variable {var_name} is not being tracked.")
+            var_names = [var_names] if isinstance(var_names, str) else var_names
+            for var_name in var_names:
+                if var_name in self.averages:
+                    self.counts[var_name] = 0
+                    self.averages[var_name] = 0.0
+                else:
+                    print(f"Variable {var_name} is not being tracked.")
